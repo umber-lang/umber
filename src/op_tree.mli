35cc408b595edbd661aa3f49b5a861d5d25f691a
James Dyer <jamesdyer199@gmail.com>
2021-01-12 03:15:44 +1000

    Extract Op_tree into a standalone module

---
+++ src/op_tree.mli
@@ -0,0 +1,15 @@
+open Names
+
+type t = (Value_name.Qualified.t, Untyped.Expr.t) Btree.t
+
+(** Re-associate the operator tree through tree rotations.
+    These constraints hold on the finished tree:
+    1. Every node has precedence less than or equal to both its childrens' precedence.
+    2. When a node has precedence equal to that some of its children,
+       it and those children all share the same associativity:
+       a. In the case of left associativity, rotating anticlockwise results in a tree
+          which violates constraint 1.
+       b. In the case of right associativity, rotating clockwise results in a tree
+          which violates constraint 1.
+       c. The case of no associativity is not allowed. *)
+val to_untyped_expr : names:Name_bindings.t -> t -> Untyped.Expr.t
