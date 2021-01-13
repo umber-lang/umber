7481afdb216f6877aab1b818544457598ce5751c
James Dyer <jamesdyer199@gmail.com>
2021-01-13 22:25:58 +1000

    Enable span output in tests

--- src/node.ml
+++ src/node.ml
@@ -13,12 +13,9 @@ let fold_map t ~f =
   x, { t with node }
 ;;
 
-(*let sexp_of_t sexp_of_node { span; node } =
+let sexp_of_t sexp_of_node { span; node } =
   Sexp.List [ Span.sexp_of_t span; sexp_of_node node ]
-;;*)
-
-(* FIXME: Revert to correct sexp conversion! *)
-let sexp_of_t sexp_of_node { node; _ } = sexp_of_node node
+;;
 
 let t_of_sexp node_of_sexp = function
   | Sexp.List [ span; node ] -> { span = Span.t_of_sexp span; node = node_of_sexp node }
