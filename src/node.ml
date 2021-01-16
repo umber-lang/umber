bdff8c8fc22eeb86b194c4810d243725680e9153
James Dyer <jamesdyer199@gmail.com>
2021-01-16 12:33:13 +1000

    Remove spans from Node sexp output

--- src/node.ml
+++ src/node.ml
@@ -1,5 +1,3 @@
-open Import
-
 type 'a t =
   { span : Span.t
   ; node : 'a
@@ -13,11 +11,5 @@ let fold_map acc t ~f =
   acc, { t with node }
 ;;
 
-let sexp_of_t sexp_of_node { span; node } =
-  Sexp.List [ Span.sexp_of_t span; sexp_of_node node ]
-;;
-
-let t_of_sexp node_of_sexp = function
-  | Sexp.List [ span; node ] -> { span = Span.t_of_sexp span; node = node_of_sexp node }
-  | _ -> raise_s [%message "Node.t_of_sexp: parse failed"]
-;;
+let sexp_of_t sexp_of_node { node; _ } = sexp_of_node node
+let t_of_sexp node_of_sexp node = { span = Span.dummy; node = node_of_sexp node }
