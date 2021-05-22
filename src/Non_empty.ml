424dde899794ae85fef535cc6a43595d633fadac
James Dyer <jamesdyer199@gmail.com>
2021-05-23 02:39:43 +0800

    Fill out Non_empty interface

--- src/Non_empty.ml
+++ src/Non_empty.ml
@@ -26,5 +26,55 @@ let rev (x :: xs) =
   loop [ x ] xs
 ;;
 
+let append (x :: xs) (y :: ys) = x :: (xs @ (y :: ys))
+let append_list (x :: xs) ys = x :: (xs @ ys)
+let rev_append xs ys = List.rev_append (to_list xs) (to_list ys) |> of_list_exn
+
+include Container.Make (struct
+  type nonrec 'a t = 'a t
+
+  let fold xs = List.fold (to_list xs)
+  let length = `Define_using_fold
+  let iter = `Define_using_fold
+end)
+
+include Monad.Make (struct
+  type nonrec 'a t = 'a t
+
+  let return x = [ x ]
+
+  let bind (x :: xs) ~f =
+    let rec loop xs ~f acc =
+      match (xs : _ list) with
+      | [] -> acc
+      | x :: xs -> loop xs ~f (rev_append (f x) acc)
+    in
+    loop xs ~f (f x) |> rev
+  ;;
+
+  let map = `Define_using_bind
+end)
+
+let zip (x :: xs) (y :: ys) =
+  let rec loop xs ys acc =
+    match xs, ys with
+    | [], _ | _, [] -> acc
+    | x :: xs, y :: ys -> loop xs ys ((x, y) :: to_list acc)
+  in
+  loop xs ys [ x, y ] |> rev
+;;
+
+let unzip ((x, y) :: xys) =
+  let rec loop xys xs ys =
+    match xys with
+    | [] -> xs, ys
+    | (x, y) :: xys -> loop xys (x :: to_list xs) (y :: to_list ys)
+  in
+  loop xys [ x ] [ y ]
+;;
+
+let is_empty _ = false
+let min_elt xs ~compare = List.min_elt (to_list xs) ~compare |> Option.value_exn
+let max_elt xs ~compare = List.max_elt (to_list xs) ~compare |> Option.value_exn
 let sexp_of_t sexp_of (x :: xs) = sexp_of_list sexp_of (x :: xs)
 let t_of_sexp of_sexp sexp = of_list_exn (list_of_sexp of_sexp sexp)
--- src/Non_empty.mli
+++ src/Non_empty.mli
@@ -1,6 +1,17 @@
+open Import
+
 type 'a t = ( :: ) of 'a * 'a list [@@deriving sexp]
+
+include Container.S1 with type 'a t := 'a t
+include Monad.S with type 'a t := 'a t
 
 val cons : 'a -> 'a t -> 'a t
 val of_list : 'a list -> 'a t option
-val to_list : 'a t -> 'a list
+val of_list_exn : 'a list -> 'a t
 val rev : 'a t -> 'a t
+val append : 'a t -> 'a t -> 'a t
+val append_list : 'a t -> 'a list -> 'a t
+val zip : 'a t -> 'b t -> ('a * 'b) t
+val unzip : ('a * 'b) t -> 'a t * 'b t
+val min_elt : 'a t -> compare:('a -> 'a -> int) -> 'a
+val max_elt : 'a t -> compare:('a -> 'a -> int) -> 'a
