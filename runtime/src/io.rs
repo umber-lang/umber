5b1a351fdb7d8341632303ff637d7de40198a033
James Dyer <jamesdyer199@gmail.com>
2023-01-08 19:49:18 +0800

    Add back rust runtime

---
+++ runtime/src/io.rs
@@ -0,0 +1,21 @@
+use crate::types::BlockPtr;
+
+#[no_mangle]
+pub extern "C" fn umber_print_int(x: BlockPtr) {
+    print!("{}", x.as_int());
+}
+
+#[no_mangle]
+pub extern "C" fn umber_print_float(x: BlockPtr) {
+    print!("{}", x.as_float());
+}
+
+#[no_mangle]
+pub extern "C" fn umber_print_string(x: BlockPtr) {
+    print!("{}", x.as_str());
+}
+
+#[no_mangle]
+pub extern "C" fn umber_print_endline(x: BlockPtr) {
+    print!("{}\n", x.as_str());
+}
