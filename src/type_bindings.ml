2474d18ccc8998f3a730db00733c287b81930664
James Dyer <jamesdyer199@gmail.com>
2023-02-20 22:38:51 +0800

    Fix bug where we would only follow aliases if comparing two s

--- src/type_bindings.ml
+++ src/type_bindings.ml
@@ -37,6 +37,21 @@ let rec unify ~names ~types t1 t2 =
     | Ok () -> ()
     | Unequal_lengths -> type_error "Type item length mismatch" t1 t2
   in
+  let instantiate_alias param_list expr =
+    let params = Type.Param.Env_to_vars.create () in
+    List.iter param_list ~f:(fun p ->
+      ignore (Type.Param.Env_to_vars.find_or_add params p : Type.Var_id.t));
+    Type.Scheme.instantiate
+      expr
+      ~params
+      ~map_name:(Name_bindings.absolutify_type_name names)
+  in
+  let get_decl names name args =
+    let decl = Name_bindings.find_absolute_type_decl names name in
+    if List.length args = Type.Decl.arity decl
+    then decl
+    else type_error "Partially applied type constructor" t1 t2
+  in
   match t1, t2 with
   | Var id1, Var id2 when Type.Var_id.(id1 = id2) -> ()
   | Var id1, _ when is_bound id1 -> unify ~names ~types (get_type id1) t2
@@ -44,62 +59,53 @@ let rec unify ~names ~types t1 t2 =
   | Var id1, _ -> set_type id1 t2
   | _, Var id2 -> set_type id2 t1
   | Type_app (name1, args1), Type_app (name2, args2) ->
-    let instantiate_alias param_list expr =
-      let params = Type.Param.Env_to_vars.create () in
-      List.iter param_list ~f:(fun p ->
-        ignore (Type.Param.Env_to_vars.find_or_add params p : Type.Var_id.t));
-      Type.Scheme.instantiate
-        expr
-        ~params
-        ~map_name:(Name_bindings.absolutify_type_name names)
-    in
-    let get_decl names name args =
-      let decl = Name_bindings.find_absolute_type_decl names name in
-      if List.length args = Type.Decl.arity decl
-      then decl
-      else type_error "Partially applied type constructor" t1 t2
-    in
     (match get_decl names name1 args1 with
-    | params, Alias expr -> unify ~names ~types (instantiate_alias params expr) t2
-    | decl1 ->
-      (match get_decl names name2 args2 with
-      | params, Alias expr -> unify ~names ~types t1 (instantiate_alias params expr)
-      | decl2 ->
-        if not (phys_equal decl1 decl2) then type_error "Type application mismatch" t1 t2;
-        iter2 args1 args2 ~f:(unify ~names ~types)))
+     | params, Alias expr -> unify ~names ~types (instantiate_alias params expr) t2
+     | decl1 ->
+       (match get_decl names name2 args2 with
+        | params, Alias expr -> unify ~names ~types t1 (instantiate_alias params expr)
+        | decl2 ->
+          if not (phys_equal decl1 decl2)
+          then type_error "Type application mismatch" t1 t2;
+          iter2 args1 args2 ~f:(unify ~names ~types)))
+  | Type_app (name, args), ((Tuple _ | Function _ | Partial_function _) as other_type)
+  | ((Tuple _ | Function _ | Partial_function _) as other_type), Type_app (name, args) ->
+    (match get_decl names name args with
+     | params, Alias expr ->
+       unify ~names ~types (instantiate_alias params expr) other_type
+     | _ -> type_error "Type application mismatch" t1 t2)
   | Function (args1, res1), Function (args2, res2) ->
     (match Nonempty.iter2 args1 args2 ~f:(unify ~names ~types) with
-    | Same_length -> ()
-    | Left_trailing _ | Right_trailing _ -> fun_arg_number_mismatch t1 t2);
+     | Same_length -> ()
+     | Left_trailing _ | Right_trailing _ -> fun_arg_number_mismatch t1 t2);
     unify ~names ~types res1 res2
   | Partial_function (args1, id1), Partial_function (args2, id2) ->
     (match Nonempty.iter2 args1 args2 ~f:(unify ~names ~types) with
-    | Left_trailing args1_trailing ->
-      unify ~names ~types (Partial_function (args1_trailing, id1)) (Var id2)
-    | Right_trailing args2_trailing ->
-      unify ~names ~types (Var id1) (Partial_function (args2_trailing, id2))
-    | Same_length -> unify ~names ~types (Var id1) (Var id2))
+     | Left_trailing args1_trailing ->
+       unify ~names ~types (Partial_function (args1_trailing, id1)) (Var id2)
+     | Right_trailing args2_trailing ->
+       unify ~names ~types (Var id1) (Partial_function (args2_trailing, id2))
+     | Same_length -> unify ~names ~types (Var id1) (Var id2))
   | Partial_function (args1, id), Function (args2, res) ->
     (match Nonempty.iter2 args1 args2 ~f:(unify ~names ~types) with
-    | Left_trailing _ -> fun_arg_number_mismatch t1 t2
-    | Right_trailing args2_trailing ->
-      let id' = Type.Var_id.create () in
-      unify ~names ~types (Var id') res;
-      unify ~names ~types (Var id) (Partial_function (args2_trailing, id'))
-    | Same_length -> unify ~names ~types (Var id) res)
+     | Left_trailing _ -> fun_arg_number_mismatch t1 t2
+     | Right_trailing args2_trailing ->
+       let id' = Type.Var_id.create () in
+       unify ~names ~types (Var id') res;
+       unify ~names ~types (Var id) (Partial_function (args2_trailing, id'))
+     | Same_length -> unify ~names ~types (Var id) res)
   | Function (args2, res), Partial_function (args1, id) ->
     (match Nonempty.iter2 args1 args2 ~f:(unify ~names ~types) with
-    | Left_trailing args1_trailing ->
-      let id' = Type.Var_id.create () in
-      unify ~names ~types res (Var id');
-      unify ~names ~types (Partial_function (args1_trailing, id')) (Var id)
-    | Right_trailing _ -> fun_arg_number_mismatch t1 t2
-    | Same_length -> unify ~names ~types res (Var id))
+     | Left_trailing args1_trailing ->
+       let id' = Type.Var_id.create () in
+       unify ~names ~types res (Var id');
+       unify ~names ~types (Partial_function (args1_trailing, id')) (Var id)
+     | Right_trailing _ -> fun_arg_number_mismatch t1 t2
+     | Same_length -> unify ~names ~types res (Var id))
   | Tuple xs, Tuple ys -> iter2 ~f:(unify ~names ~types) xs ys
-  | Type_app _, (Tuple _ | Function _ | Partial_function _)
-  | Tuple _, (Type_app _ | Function _ | Partial_function _)
-  | Function _, (Type_app _ | Tuple _)
-  | Partial_function _, (Type_app _ | Tuple _) -> type_error "Types do not match" t1 t2
+  | Tuple _, (Function _ | Partial_function _)
+  | Function _, Tuple _
+  | Partial_function _, Tuple _ -> type_error "Types do not match" t1 t2
 ;;
 
 let rec substitute types typ =
@@ -107,8 +113,8 @@ let rec substitute types typ =
     match typ with
     | Var id ->
       (match Hashtbl.find types.vars id with
-      | Some type_sub -> Retry type_sub
-      | None -> Halt typ)
+       | Some type_sub -> Retry type_sub
+       | None -> Halt typ)
     | Partial_function (args, id) -> combine_partial_functions types typ args id
     | _ -> Defer typ)
 
@@ -118,13 +124,13 @@ and combine_partial_functions types typ args id =
   | Some type_sub ->
     let args = Nonempty.map args ~f:(substitute types) in
     (match substitute types type_sub with
-    | Partial_function (args', id') ->
-      let args' = Nonempty.map args' ~f:(substitute types) in
-      let args_combined = Nonempty.(args @ args') in
-      let typ = Type.Expr.Partial_function (args_combined, id') in
-      combine_partial_functions types typ args_combined id'
-    | (Var _ | Type_app _ | Tuple _ | Function _) as type_sub ->
-      Halt (Function (args, type_sub)))
+     | Partial_function (args', id') ->
+       let args' = Nonempty.map args' ~f:(substitute types) in
+       let args_combined = Nonempty.(args @ args') in
+       let typ = Type.Expr.Partial_function (args_combined, id') in
+       combine_partial_functions types typ args_combined id'
+     | (Var _ | Type_app _ | Tuple _ | Function _) as type_sub ->
+       Halt (Function (args, type_sub)))
 ;;
 
 let generalize types typ =
@@ -133,9 +139,10 @@ let generalize types typ =
     (substitute types typ)
     ~var:(Type.Param.Env_of_vars.find_or_add env)
     ~pf:(never_happens [%here])
-    ~f:(function
-      | Partial_function (args, id) -> Defer (Function (args, Var id))
-      | typ -> Defer typ)
+    ~f:
+      (function
+       | Partial_function (args, id) -> Defer (Function (args, Var id))
+       | typ -> Defer typ)
 ;;
 
 (* FIXME: add unit test for cyclic type variables. Also consider if we want to implement
