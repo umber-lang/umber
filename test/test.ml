1fa03acc4edc8920b819ad2e234755a5988d9c8c
James Dyer <jamesdyer199@gmail.com>
2025-05-02 22:25:55 +0800

    Enable an effects test for the backend

--- test/test.ml
+++ test/test.ml
@@ -16,7 +16,6 @@ let should_type_check test = not (List.mem ~equal:String.equal parse_only_tests 
 (* TODO: enable these for mir. *)
 let type_only_tests =
   [ "LetPattern" (* unions in toplevel let bindings *)
-  ; "Effects" (* effects *)
   ; "ReraiseExn" (* effects *)
   ; "State" (* effects *)
   ; "Iter" (* effects *)
